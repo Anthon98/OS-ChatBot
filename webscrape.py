9e72929dca0c544d97b6ed1ddc6023b077819680
Anthon98 <anthon.kallankari@gmail.com>
2021-10-27 15:19:40 +0300

    New feature but it's not perfect.
    New features but not perfect.

--- webscrape.py
+++ webscrape.py
@@ -5,6 +5,8 @@ import requests
 import argparse
 import logging as logger
 import json
+import time
+import re
 
 from pprint import pprint
 from bs4 import BeautifulSoup
@@ -18,6 +20,12 @@ class ChatBot:
     def __init__(self: '__main__.ChatBot', username: str, password: str) -> None:
         self.username = username
         self.password = password
+        self.urls = {
+            'index': 'https://onlinesequencer.net/forum/index.php',
+            'login': 'https://onlinesequencer.net/ajax/login.php',
+            'member': 'https://onlinesequencer.net/forum/member.php',
+            'send_chat': 'https://onlinesequencer.net/forum/xmlhttp.php?action=ajaxchat_send',
+        }
         self.handle()
 
     def __repr__(self: '__main__.ChatBot') -> str:
@@ -28,19 +36,27 @@ class ChatBot:
         # To be made.
         return '__str__ for ChatBot'
 
+    def fetch_my_post_key(self: '__main__.ChatBot') -> None:
+        # We need the default headers for the validation call.
+        self.session.headers = self.default_header
+        get_my_post_key = self.session.get(self.urls['login'])
+
+        if not get_my_post_key.text == "false":
+            response_json = json.loads(get_my_post_key.text)
+            self.my_post_key = response_json['post_key']
+        else:
+            get_my_post_key = self.session.get(self.urls['index'])
+            self.my_post_key = re.search('var my_post_key = "(.*)";',
+                                         get_my_post_key.text).group(1)
+
+        logger.info('Current validated my_post_key: %s' % self.my_post_key)
+
     def login(self: '__main__.ChatBot', session) -> Tuple['requests.sessions.Session', str]:
         logger.info("Logging in...")
-        url = 'https://onlinesequencer.net/forum/member.php'
-        # Dummy call to get the post key. (get or post here wont matter)
-        get_my_post_key = session.get(url)
 
-        # Generate an lxml format and parse through with bs4, this isn't a request
-        # It just finds and gets values much like regex in the actual response.
-        # Because usually these 'challenge' validator types are embedded in the page.
-        # Not bad practice by any means.
-        soup = BeautifulSoup(get_my_post_key.text, 'lxml')
-        my_post_key = soup.find(
-            'input', attrs={'name': 'my_post_key', 'type': 'hidden'}).get('value')
+        self.session = session
+        self.default_header = self.session.headers
+        self.fetch_my_post_key()
 
         data = {
             'action': 'do_login',
@@ -48,17 +64,25 @@ class ChatBot:
             'url': '/ajax/login.php',
             'username': self.username,
             'password': self.password,
-            'my_post_key': my_post_key,
+            'my_post_key': self.my_post_key,
         }
 
-        response = session.post(url, data=data)
-        response_json = json.loads(response.text)
-        my_post_key = response_json['post_key']
+        print(data['my_post_key'])
+
+        # In case of an invalid my_post_key, this will fail.
+
+        try:
+            response = self.session.post(self.urls['member'], data=data)
+            response_json = json.loads(response.text)
+            self.my_post_key = response_json['post_key']
+        except Exception as e:
+            logger.info("TEST???")
+            logger.error(e)
 
         # Format our cookie for the header.
-        cookie = ''
-        for k, v in session.cookies.items():
-            cookie += '%s=%s; ' % (k, v)
+        self.cookie = ''
+        for k, v in self.session.cookies.items():
+            self.cookie += '%s=%s; ' % (k, v)
 
         header = {
             'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
@@ -68,36 +92,33 @@ class ChatBot:
             'Accept': '*/*',
             'Accept-Encoding': 'gzip, deflate, br',
             'Connection': 'keep-alive',
-            'Cookie': cookie
+            'Cookie': self.cookie
         }
 
         # Replace default headers with our new one.
-        session.headers = header
+        self.main_header = header
+        self.session.headers = self.main_header
 
-        return session, my_post_key
-
-    def send_message(self: '__main__.ChatBot', session: 'requests.sessions.Session', my_post_key: str, message: str) -> None:
-        send_chat = 'https://onlinesequencer.net/forum/xmlhttp.php?action=ajaxchat_send'
-
+    def send_message(self: '__main__.ChatBot', message: str) -> None:
         chat_data = {
-            'my_post_key': my_post_key,
+            'my_post_key': self.my_post_key,
             'context': '0',
             'message': message,
         }
 
-        send = session.post(send_chat, data=chat_data)
-        pprint(send.text)
+        self.session.post(self.urls['send_chat'], data=chat_data)
+        # pprint(send.text)
 
     def handle(self: '__main__.ChatBot') -> None:
         # Due to 302 redirect we need to use sessions.
         with requests.session() as session:
-            session, my_post_key = self.login(session=session)
+            self.login(session=session)
 
-            for _ in range(1):
+            for _ in range(20):
                 inpt = str(input("Chat Message: "))
 
-                self.send_message(session=session,
-                                  my_post_key=my_post_key, message=inpt)
+                self.fetch_my_post_key()
+                self.send_message(message=inpt)
 
 
 if __name__ == "__main__":
